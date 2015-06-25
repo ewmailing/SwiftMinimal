47a0fe011b8c92f594cdb18b87e367cea45ff503
Eric Wing <ewing -dot- public |-at-| gmail.com>
2015-06-25 05:40:11 -0700

    Initial Commit

---
+++ MinimalSwift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MinimalSwift
+//
+//  Created by Eric Wing on 6/25/15.
+//  Copyright (c) 2015 PlayControl Software LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ MinimalSwiftTests/MinimalSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  MinimalSwiftTests.swift
+//  MinimalSwiftTests
+//
+//  Created by Eric Wing on 6/25/15.
+//  Copyright (c) 2015 PlayControl Software LLC. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class MinimalSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
